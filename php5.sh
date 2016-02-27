1c58f29f9762f836ed86205ab0b6550dffa840f7
Michael Kenney <michael.kenney@returnpath.net>
2016-02-27 12:48:18 -0700

    Adding shell scripts

---
+++ php5.sh
@@ -0,0 +1,8 @@
+#!/bin/env sh
+
+container_name=composer-temp
+if [[ $(docker ps -a | grep $container_name) ]]; then
+    docker rm -f $container_name > /dev/null
+fi
+docker run --rm -i -v $(pwd):/app:rw mkenney/php-cli $@
+
